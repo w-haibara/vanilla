241a68cab50643c3a58d7a9d2e96887c891ac620
w-haibara <hwhaibarawataru@gmail.com>
2020-07-19 22:32:48 +0900

    fix typo

--- test/test-data-gen.go
+++ test/test-data-gen.go
@@ -18,7 +18,7 @@ func gen(key, iv []byte, msgs []string) {
 func main() {
 	key := []byte("1234567890123456")
 	iv := []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
-	msgs := []string{"Hello", "Hello Worls!", "Who are you?", "Golang", "Goodbye"}
+	msgs := []string{"Hello", "Hello World!", "Who are you?", "Golang", "Goodbye"}
 
 	gen(key, iv, msgs)
 }
